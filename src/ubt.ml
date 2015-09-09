1a2dfcc8ca2f3b1376858ed1211c29b90cf5e2ef
Benedikt Schmidt <beschmi@gmail.com>
2015-09-09 17:45:09 +0200

    fix merge

--- src/ubt.ml
+++ src/ubt.ml
@@ -1,5 +1,6 @@
 (* Main function of the unbounded tool *)
 
+open Core_kernel.Std
 open Abbrevs
 
 let input_file filename =
@@ -13,15 +14,15 @@ let input_file filename =
   in
   let lines = go [] in
   let _ = close_in_noerr in_channel in
-  String.concat "\n" (L.rev lines)
+  String.concat ~sep:"\n" (L.rev lines)
        
 let main =
   if Array.length Sys.argv <> 3 then
     output_string stderr (F.sprintf "usage: %s <definition_file> <instructions_file / 'automatic'> \n" Sys.argv.(0))
   else
     let scmds = input_file Sys.argv.(1) in
-    if (String.compare Sys.argv.(2) "automatic") == 0 then
-      let filename = Str.string_before Sys.argv.(1) ((String.length Sys.argv.(1)) - 4) in
+    if Sys.argv.(2) = "automatic" then
+      let filename = String.prefix Sys.argv.(1) ((String.length Sys.argv.(1)) - 4) in
       Analyze.automatic_tool scmds (filename ^ ".prf")
     else
       let instrs = input_file Sys.argv.(2) in
